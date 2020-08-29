7b5377296826546d3cab77f8d9f78e274a19dd6a
lganwebb <lganwebb04@gmail.com>
2020-08-29 18:15:07 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 version = ""
-with open("discord/ext/template/__init__.py") as f:
+with open("discord/ext/ipc/__init__.py") as f:
     version = re.search(r"^__version__\s*=\s*[\'\"]([^\'\"]*)[\'\"]", f.read(), re.MULTILINE).group(1)
 
-with open("README.rst", encoding="utf-8") as f:
+with open("README.rst", "r") as f:
     readme = f.read()
 
 setup(
@@ -20,8 +20,8 @@ setup(
     packages=["discord.ext.ipc"],
     license="Apache Software License",
     description="An IPC extension allowing for the communication between a discord.py bot and an asynchronous web-framework (i.e. Quart or aiohttp.web)",
-    long_description=readme,
     long_description_content_type="text/x-rst",
+    long_description=readme,
     include_package_data=True,
     install_requires=requirements,
     classifiers=[
