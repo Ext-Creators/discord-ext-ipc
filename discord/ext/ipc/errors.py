3c8ef0706a30b2fc39096baddab686e71159f847
Lgan <46571352+lganWebb@users.noreply.github.com>
2020-10-01 14:52:10 +0100

    Update errors.py

--- discord/ext/ipc/errors.py
+++ discord/ext/ipc/errors.py
@@ -1,5 +1,7 @@
 class NoEndpointFoundError(Exception):
+    """Raised upon requesting an invalid endpoint"""
     pass
 
 class ServerConnectionRefusedError(Exception):
+    """Raised upon a server refusing to connect / not being found"""
     pass
