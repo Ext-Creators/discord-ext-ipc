beaaccbcd250b487eb0d5d6f80af3c4578302a66
skelmis <skelmis.craft@gmail.com>
2021-02-05 01:55:19 +1300

    Remove the license from the documentation pages

--- discord/ext/ipc/client.py
+++ discord/ext/ipc/client.py
@@ -1,4 +1,5 @@
 """
+LICENSE
     Copyright 2021 Ext-Creators
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -9,6 +10,7 @@
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
+LICENSE
 """
 
 import asyncio
--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,8 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+from sphinx.ext.autodoc import between
+
 
 # -- Project information -----------------------------------------------------
 
@@ -52,3 +54,10 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+def setup(app):
+    # Register a sphinx.ext.autodoc.between listener to ignore everything
+    # between lines that contain the word LICENSE
+    app.connect("autodoc-process-docstring", between("^.*LICENSE.*$", exclude=True))
+    return app
