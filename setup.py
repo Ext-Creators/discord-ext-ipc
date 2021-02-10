8082db3ef0b1e4b3a615236e5b6c00e67e49ebc4
skelmis <skelmis.craft@gmail.com>
2021-02-11 03:26:29 +1300

    Blacked code

--- setup.py
+++ setup.py
@@ -31,11 +31,7 @@ classifiers = [
 ]
 
 extras_require = {
-    "docs": [
-        "sphinx",
-        "sphinxcontrib_trio",
-        "sphinx-rtd-theme",
-    ],
+    "docs": ["sphinx", "sphinxcontrib_trio", "sphinx-rtd-theme",],
 }
 
 project_urls = {
