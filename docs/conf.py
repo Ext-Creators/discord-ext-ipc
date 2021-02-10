4858e2b37203e07dfa9248de7797620c8dba255a
skelmis <skelmis.craft@gmail.com>
2021-02-10 23:12:52 +1300

    Clean conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,63 +1,40 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
 
-# -- Path setup --------------------------------------------------------------
+sys.path.insert(0, os.path.abspath(".."))
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-from sphinx.ext.autodoc import between
+from discord.ext.ipc import __version__ as version
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'discord-ext-ipc'
-copyright = '2021, Ext-Creators'
-author = 'Ext-Creators'
+project = "discord-ext-ipc"
+copyright = "2021, Ext-Creators"
+author = "Ext-Creators"
 
 # The full version, including alpha/beta/rc tags
-release = 'v2.0.0a'
+release = version
 
 
 # -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx-rtd-theme",
+    "sphinxcontrib_trio",
+]
 
 
-# -- Options for HTML output -------------------------------------------------
+html_theme = "sphinx_rtd_theme"
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'sphinx_rtd_theme'
+autodoc_typehints = "none"
+intersphinx_mapping = {
+    "aiohttp": ("https://docs.aiohttp.org/en/stable/", None),
+    "python": ("https://docs.python.org/3", None),
+}
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
-
-def setup(app):
-    # Register a sphinx.ext.autodoc.between listener to ignore everything
-    # between lines that contain the word DOC_IGNORE
-    app.connect("autodoc-process-docstring", between("^.*DOC_IGNORE.*$", exclude=True))
-    return app
+highlight_language = "python3"
+master_doc = "index"
+pygments_style = "friendly"
+source_suffix = ".rst"
--- docs/index.rst
+++ docs/index.rst
@@ -1,8 +1,3 @@
-.. discord-ext-ipc documentation master file, created by
-   sphinx-quickstart on Fri Feb  5 01:45:00 2021.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
 Welcome to discord-ext-ipc's documentation!
 ===========================================
 
