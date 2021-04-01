55f441d29222b46c179cb3dab632ba54f311a9ce
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2021-04-01 10:07:40 +0100

    update sphinx configuration
    should appease deployment

--- docs/conf.py
+++ docs/conf.py
@@ -1,13 +1,33 @@
 import re
 
 project = "discord-ext-ipc"
-copyright = "2021, Ext-Creators"
+copyright = "2020-present, Ext-Creators"
 author = "Ext-Creators"
 
+_version_regex = r"^version = ('|\")((?:[0-9]+\.)*[0-9]+(?:\.?([a-z]+)(?:\.?[0-9])?)?)\1$"
+
 with open("../discord/ext/ipc/__init__.py") as stream:
-    release = version = re.search(
-        r"^__version__\s*=\s*[\'\"]([^\'\"]*)[\'\"]", stream.read(), re.MULTILINE
-    ).group(1)
+    match = re.search(_version_regex, stream.read(), re.MULTILINE)
+
+version = match.group(2)
+
+if match.group(3) is not None:
+    try:
+        import subprocess
+
+        process = subprocess.Popen(["git", "rev-list", "--count", "HEAD"], stdout=subprocess.PIPE)
+        out, _ = process.communicate()
+        if out:
+            version += out.decode("utf-8").strip()
+
+        process = subprocess.Popen(["git", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE)
+        out, _ = process.communicate()
+        if out:
+            version += "+g" + out.decode("utf-8").strip()
+    except (Exception) as e:
+        pass
+
+release = version
 
 
 extensions = [
@@ -19,9 +39,8 @@ extensions = [
 ]
 
 
-html_theme = "sphinx_rtd_theme"
-
 autodoc_typehints = "none"
+
 intersphinx_mapping = {
     "aiohttp": ("https://docs.aiohttp.org/en/stable/", None),
     "python": ("https://docs.python.org/3", None),
@@ -29,6 +48,7 @@ intersphinx_mapping = {
 }
 
 highlight_language = "python3"
+html_theme = "sphinx_rtd_theme"
 master_doc = "index"
 pygments_style = "friendly"
 source_suffix = ".rst"
