e1906a7b8c34e1339f1fcdaad79b9e35e9ae11fa
Logan <46571352+lganWebb@users.noreply.github.com>
2021-04-24 18:56:57 +0100

    Appease linter in setup.py

--- setup.py
+++ setup.py
@@ -43,7 +43,9 @@ project_urls = {
     "Source": "https://github.com/Ext-Creators/discord-ext-ipc",
 }
 
-_version_regex = r"^version = ('|\")((?:[0-9]+\.)*[0-9]+(?:\.?([a-z]+)(?:\.?[0-9])?)?)\1$"
+_version_regex = (
+    r"^version = ('|\")((?:[0-9]+\.)*[0-9]+(?:\.?([a-z]+)(?:\.?[0-9])?)?)\1$"
+)
 
 with open("discord/ext/ipc/__init__.py") as stream:
     match = re.search(_version_regex, stream.read(), re.MULTILINE)
@@ -54,12 +56,16 @@ if match.group(3) is not None:
     try:
         import subprocess
 
-        process = subprocess.Popen(["git", "rev-list", "--count", "HEAD"], stdout=subprocess.PIPE)
+        process = subprocess.Popen(
+            ["git", "rev-list", "--count", "HEAD"], stdout=subprocess.PIPE
+        )
         out, _ = process.communicate()
         if out:
             version += out.decode("utf-8").strip()
 
-        process = subprocess.Popen(["git", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE)
+        process = subprocess.Popen(
+            ["git", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE
+        )
         out, _ = process.communicate()
         if out:
             version += "+g" + out.decode("utf-8").strip()
