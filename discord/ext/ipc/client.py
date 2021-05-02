6084bb7e8642784853c31a1697c08421b3d6ab70
Logan <46571352+lganWebb@users.noreply.github.com>
2021-05-02 20:29:19 +0100

    Remove retry delay

--- discord/ext/ipc/client.py
+++ discord/ext/ipc/client.py
@@ -132,11 +132,10 @@ class Client:
 
         if recv.type == aiohttp.WSMsgType.CLOSED:
             log.error(
-                "WebSocket connection unexpectedly closed. IPC Server is unreachable. Attempting reconnection in 5 seconds."
+                "WebSocket connection unexpectedly closed. IPC Server is unreachable."
             )
 
             await self.session.close()
-            await asyncio.sleep(5)
             await self.init_sock()
 
             return await self.request(endpoint, **kwargs)
